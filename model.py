9476946e0657fb1fd2dfe98184658f4a4b4498c0
ayush <ayushkumarsingh97@gmail.com>
2016-07-23 13:32:13 +0530

    major cleanng done

--- model.py
+++ model.py
@@ -8,8 +8,59 @@ from sklearn import cross_validation
 from sklearn.cross_validation import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
-from sklearn.metrics.metrics import accuracy_score,classification_report
+# from sklearn.metrics.metrics import accuracy_score,classification_report
 from sklearn.ensemble import RandomForestClassifier
 from random import randint
 from scipy.stats import pointbiserialr, spearmanr
 from sklearn.svm import SVC
+from sklearn import preprocessing
+
+def feature_selection():
+    df = pd.read_csv("dataset/train_new.csv")
+    df = df.dropna()
+    print df.describe()
+    y = df["Business_Sourced"]
+    print y.shape
+    df = df.drop(['ID','Business_Sourced'],axis=1)
+
+    param_df=df.columns.values
+    print df
+    print param_df
+    # param_df=['Applicant_Gender','Applicant_Occupation','Manager_Joining_Designation'
+    # ,'Manager_Current_Designation','Manager_Status','Manager_Gender'
+    # ,'Manager_exp','App_age','Manager_age']
+
+    scores = []
+    scoreCV =[]
+    # df = df[np.isfinite(df['EPS'])]
+    # for j in range(5):
+        # scores = []
+        # scoreCV=[]
+        # for i in range(0,len(param_df)-1):
+    for i in range(len(param_df)):
+        print i
+        # print df[:,0:i+1:]
+        # X = df.ix[:,0:(i+1)]
+        X = df.ix[:,i:(i+1)]
+
+        X = preprocessing.scale(X)
+        print X.shape
+
+        # print X
+        clf = LogisticRegression()
+        # clf = RandomForestClassifier(n_estimators=1000)
+        # clf = DecisionTreeClassifier(criterion='gini')
+        scoreCV = cross_validation.cross_val_score(clf, X, y, cv=3)
+
+        print np.mean(scoreCV)
+        scores.append(np.mean(scoreCV))
+
+    plt.figure(figsize=(15,5))
+    plt.plot(range(1,len(scores)+1),scores, '.-')
+    plt.axis("tight")
+    plt.title('Feature Selection', fontsize=14)
+    plt.xlabel('# Features', fontsize=12)
+    plt.ylabel('Score', fontsize=12)
+    plt.show()
+
+feature_selection()
--- smart.py
+++ smart.py
@@ -3,8 +3,17 @@ from scipy.stats import pointbiserialr, spearmanr
 from datetime import datetime
 import numpy as np
 
-def load_train():
-    df =  pd.read_csv("dataset/Train_pjb2QcD.csv")
+columns = ['ID','Office_PIN','Application_Receipt_Date','Applicant_City_PIN',
+'Applicant_Gender','Applicant_BirthDate','Applicant_Marital_Status',
+'Applicant_Occupation','Applicant_Qualification','Manager_DOJ',
+'Manager_Joining_Designation','Manager_Current_Designation',
+'Manager_Grade','Manager_Status','Manager_Gender','Manager_DoB',
+'Manager_Num_Application','Manager_Num_Coded','Manager_Business',
+'Manager_Num_Products','Manager_Business2','Manager_Num_Products2',
+'Business_Sourced']
+
+def load_train(filename,dest):
+    df =  pd.read_csv(filename)
     # print df.describe()
 
 
@@ -20,14 +29,6 @@ def load_train():
     # # print df[df["Applicant_Gender"]=="M"][df["Business_Sourced"]==0].describe()
     # print df[df["Applicant_Gender"]=="M"].describe()
 
-    columns = ['ID','Office_PIN','Application_Receipt_Date','Applicant_City_PIN',
-    'Applicant_Gender','Applicant_BirthDate','Applicant_Marital_Status',
-    'Applicant_Occupation','Applicant_Qualification','Manager_DOJ',
-    'Manager_Joining_Designation','Manager_Current_Designation',
-    'Manager_Grade','Manager_Status','Manager_Gender','Manager_DoB',
-    'Manager_Num_Application','Manager_Num_Coded','Manager_Business',
-    'Manager_Num_Products','Manager_Business2','Manager_Num_Products2',
-    'Business_Sourced']
 
     f = ["Applicant_Gender","Applicant_Marital_Status","Applicant_Qualification","Applicant_Occupation","Manager_Joining_Designation","Manager_Current_Designation","Manager_Grade","Manager_Status","Manager_Gender"]
     for k in f:
@@ -41,10 +42,28 @@ def load_train():
     df["App_age"] = df.apply(ret_app_age, axis=1)
     df["Manager_age"] = df.apply(ret_man_age, axis=1)
 
-    df = df.drop(["Applicant_BirthDate","Application_Receipt_Date","Manager_DoB","Manager_DOJ"],axis=1)
+    # print df.describe()
 
-    print df.head()
-    df.to_csv("dataset/train_new.csv",index=False)
+    print df["App_age"].mean()
+    df['App_age'] = df['App_age'].fillna(df["App_age"].mean())
+    print df["Manager_age"].mean()
+    df['Manager_age'] = df['Manager_age'].fillna(df["Manager_age"].mean())
+    df['Manager_exp'] = df['Manager_exp'].fillna(df["Manager_exp"].mean())
+    df['Applicant_Qualification'] = df['Applicant_Qualification'].fillna(1)
+    df['Applicant_Gender'] = df['Applicant_Gender'].fillna(0)
+    df['Manager_Gender'] = df['Manager_Gender'].fillna(0)
+    df['Manager_Status'] = df['Manager_Status'].fillna(0)
+    df['Applicant_Occupation'] = df['Applicant_Occupation'].fillna(2)
+
+    c = ['Manager_Num_Application','Manager_Num_Coded','Manager_Business','Manager_Num_Products','Manager_Business2','Manager_Num_Products2']
+    for i in c:
+        df[i] = df[i].fillna(df[i].mean())
+
+    df = df.drop(["Applicant_BirthDate","Application_Receipt_Date","Manager_DoB","Manager_DOJ","Applicant_City_PIN","Manager_Grade","Office_PIN","Applicant_Marital_Status","Manager_Num_Coded","Manager_Num_Products","Manager_Num_Products2","Manager_Current_Designation","Manager_Joining_Designation","Manager_exp"],axis=1)
+
+    print df.columns.values
+    print df.describe()
+    df.to_csv(dest,index=False)
 
     # for i in columns:
     #     print i
@@ -84,7 +103,7 @@ def ret_man_age(df):
         date_for = "%m/%d/%Y"
         a = datetime.strptime(df["Application_Receipt_Date"],date_for)
         # print a
-        b = datetime.strptime(df["Applicant_BirthDate"],date_for)
+        b = datetime.strptime(df["Manager_DoB"],date_for)
         # print b
         d = a-b
         # print (d.days)/365.0
@@ -94,18 +113,18 @@ def ret_man_age(df):
 
 def correlation():
     df =  pd.read_csv("dataset/train_new.csv")
-    df = df.dropna(axis=0,how="any")
+    # df = df.dropna(axis=0,how="any")
     print df.describe()
-    print df.head()
+    # print df.head()
     param=[]
     correlation=[]
     abs_corr=[]
     covariance = []
-    columns = ["Applicant_Gender","App_age","Applicant_Occupation","Applicant_Qualification","Applicant_Marital_Status","Manager_age","Manager_exp","Manager_Grade","Manager_Status","Manager_Gender","Manager_Business","Manager_Business2","Manager_Num_Coded","Manager_Num_Products","Manager_Num_Products2","Manager_Num_Application","Manager_Current_Designation"]
+    columns = ["Applicant_Gender","App_age","Applicant_Occupation","Applicant_Qualification","Manager_age","Manager_Status","Manager_Gender","Manager_Business","Manager_Business2","Manager_Num_Application"]
     for c in columns:
         #Check if binary or continuous
 
-        if len(df[c].unique())<=2:
+        if len(df[c].unique())<=12:
             corr = spearmanr(df['Business_Sourced'],df[c])[0]
             print "spear",c,corr
             y = df['Business_Sourced']
@@ -125,6 +144,15 @@ def correlation():
         # covariance.append(covar[0][1])
     print covariance
 
+def ana_test():
+    df = pd.read_csv("dataset/test_new.csv")
+    print df.describe()
+    # for i in columns[:-1]:
+    #     print i
+    #     print df[i].describe()
+    #     print df[i].unique().size
 if __name__ == "__main__":
-    # load_train()
-    correlation()
+    # load_train("dataset/Test_wyCirpO.csv","dataset/test_new.csv")
+    # load_train("dataset/Train_pjb2QcD.csv","dataset/train_new.csv")
+    # correlation()
+    ana_test()
